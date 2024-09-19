af5bcb079fda20b2a0229d21e40895ccc8b60f95
Sudharsan V <176049812+Sudharsanv151@users.noreply.github.com>
2024-09-19 20:44:45 +0530

    Portfolio enhancements

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -5,22 +5,43 @@ import Java from '../img/java.png'
 
 const Home=()=>{
     return(
-        <div className="bg-gradient-to-r h- to-[#1F1C18] from-[#8E0E00] ">
+        <div className="">
         <Navbar />
-        <div className="p-10 h-screen text-white ">
-        <h1 className="h-1/4 text-4xl text-center font-semibold ">ABOUT ME</h1>
-        <div className="w-[100%] text-white flex items-center gap-10">
-        <p className="text-2xl flex-row text-justify font-mono h-4/5 w-4/5 ">  Hi I am Sudharsan V, a Computer Science Engineering Student at Sri Shakthi Institute of Engineering and Technology, Coimbatore. I am a proficient Coding Enthusiast and a learning Full Stack Web Developer. I am good at Java, MySQL, OpenCV, and Web development (HTML, CSS, JavaScript). I have completed various Courses and done 5 projects</p>
-        <div className="flex justify-center items-center  h-3/5 w-3/5">
-            <img src={Prpic} alt="Sudharsan V" className="rounded-full h-1/3  w-1/3"></img>
-        </div>
-        </div>      
-        <h1 className="h-1/4 text-4xl text-center font-semibold p-48">MY SKILLS </h1>
-        <div className="w-[100%] text-white flex items-center ">
-            <div className="flex justify-center flexx-row items-center h-3/5 w-3/5 gap-14">
-            <img src={Java} alt="Java" className="rounded-full h-1/3 w-1/3"></img>
-            </div>
-        </div>
+        <div className="p-10 h-screen text-black ">    
+        {/* <div className="container h-2/5 w-2/4 my-20 ml-auto shadow-2xl rounded-3xl bg-gray-50 p-15 content-center ">
+        <h1 className="text-4xl text-center font-semibold my-auto font-sans">ABOUT ME</h1>
+            <p className=" text-xl  p-3  text-justify  m-4 font-sans">
+            Hi I am Sudharsan V, a highly motivated Computer Science student with hands-on experience in MERN stack development, web development, machine
+            learning, and computer vision. Proficient in Java, Python, and MySQL. Eager to leverage technical expertise and leadership
+            experience to contribute to cutting-edge IT solutions
+            </p>
+            {/* <img src={Prpic} alt="profile-pic"className="h-1/4 w-1/4  justify-center flex"></img> */}  
+        {/* </div> */} 
+        <div class="w-2/4 h-2/5 py-8 my-20 ml-auto p-15 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 grid grid-col-2 justify-center p-4 gap-4 rounded-lg shadow-md">
+  <div class="col-span-2 text-3xl font-bold capitalize rounded-md">
+    ABOUT ME 
+  </div>
+  <div class="col-span-2 rounded-md">
+  Hi I am Sudharsan V, a highly motivated Computer Science student with hands-on experience in MERN stack development, web development, machine
+            learning, and computer vision. Proficient in Java, Python, and MySQL. Eager to leverage technical expertise and leadership
+            experience to contribute to cutting-edge IT solutions.
+  </div>
+  <div class="col-span-1 flex-row">
+    <a href="https://sudharsanv151.github.io/" target="_blank">
+        <button class="rounded-md bg-slate-300 mr-20 flex-row flex hover:bg-slate-600 hover:text-slate-200 duration-300 p-2">
+            <p className="mr-2">Resume</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+            </svg>
+        </button>
+    </a>
+</div>
+
+  
+
+  </div>
         </div>
         </div>
     );
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -4,15 +4,15 @@ import Pricon from '../img/user.png'
 const Navbar = () => {
     return (
      <>
-      <nav className="h-20 w-full sticky flex top-0 justify-center rounded-lg bg-gradient-to-r to-[#1F1C18] from-[#8E0E00]">
-        <div className="container flex justify-between items-center">
-          <div className="text-white text-2xl font-bold">
+      <nav className="h-20 w-full sticky flex top-0 justify-center rounded-lg ">
+        <div className="container flex justify-between items-center ">
+          <div className="text-black text-2xl font-bold">
             <h1> Portfolio</h1>
           </div>
           <ul className="hidden md:flex space-x-6">
-            <li><a href="#home" className="text-white hover:text-red-400">Home</a></li>
-            <li><a href="#projects" className="text-white hover:text-red-400">Projects</a></li>
-            <li><a href="#contact" className="text-white hover:text-red-400">Contact</a></li>
+            <li><a href="#home" className="text-black hover:text-red-400">Home</a></li>
+            <li><a href="#projects" className="text-black hover:text-red-400">Projects</a></li>
+            <li><a href="#contact" className="text-black hover:text-red-400">Contact</a></li>
             <li><img src={Pricon} alt="pr" ></img></li>
           </ul>
         </div>
